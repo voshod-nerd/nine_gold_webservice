2bdb80cea384d6b1d186b4e01e54eb6eabd71c0c
Sokolov Oleg <voshod-nerd@hotmail.com>
2017-08-07 13:22:47 +0600

    1sh fixation 07082017

---
+++ src/main/java/com/mycompany/nine_gold_web_services/controller/GreetingController.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.nine_gold_web_services.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+/**
+ *
+ * @author ПК
+ */
+@Controller
+public class GreetingController {
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+}
---
+++ src/main/java/com/mycompany/nine_gold_web_services/main/Application.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.nine_gold_web_services.main;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ *
+ * @author ПК
+ */
+@SpringBootApplication
+public class Application {
+     public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
